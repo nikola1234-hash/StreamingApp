43765f2c5e4b7a1fb742e1f6d71ba307885706ae
DAVIESMEYER\nsavic <nikola.savic@daviesmeyer.com>
2023-08-09 17:44:00 +0200

    init

---
+++ Controllers/FileController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StreamingApp.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+        private readonly string _fileDownloadPath;
+
+        public FileController(IConfiguration configuration)
+        {
+            _fileDownloadPath = configuration.GetValue<string>("FileDownloadPath");
+        }
+
+
+        [HttpGet("{fileName}")]
+        public IActionResult Download(string fileName)
+        {
+            var filepath = Path.Combine(_fileDownloadPath, fileName);
+            if (!System.IO.File.Exists(filepath))
+                return NotFound();
+
+            var memory = new MemoryStream();
+            using (var stream = new FileStream(filepath, FileMode.Open))
+            {
+                stream.CopyTo(memory);
+            }
+            memory.Position = 0;
+
+            return File(memory, GetContentType(filepath), Path.GetFileName(filepath));
+        }
+
+        private string GetContentType(string path)
+        {
+            var types = GetMimeTypes();
+            var ext = Path.GetExtension(path).ToLowerInvariant();
+            return types[ext];
+        }
+
+        private Dictionary<string, string> GetMimeTypes()
+        {
+            return new Dictionary<string, string>
+            {
+                {".txt", "text/plain"},
+                {".jpg", "image/jpeg"},
+                {".jpeg", "image/jpeg"},
+           
+            };
+        }
+    }
+}
---
+++ Controllers/StreamController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StreamingApp.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class StreamController : ControllerBase
+    {
+        private readonly string _videoStreamPath;
+
+        public StreamController(IConfiguration configuration)
+        {
+            _videoStreamPath = configuration.GetValue<string>("VideoStreamPath");
+        }
+
+        [HttpGet("{videoName}")]
+        public IActionResult StreamVideo(string videoName)
+        {
+            var filepath = Path.Combine(_videoStreamPath, videoName);
+            if (!System.IO.File.Exists(filepath))
+                return NotFound();
+
+            return new FileStreamResult(new FileStream(filepath, FileMode.Open, FileAccess.Read), "video/mp4");
+        }
+    }
+}
